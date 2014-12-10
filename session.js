b05b313ad55966ea44482be22d6cfb223d644abf
Brylie Christopher Oxley <brylie@gnumedia.org>
2014-12-10 15:28:40 +0200

    Got score working.

--- session.js
+++ session.js
@@ -1,37 +1,54 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-  
-  Meteor.loginVisitor();
-  console.log(Meteor.userId());
+    // counter starts at 0
+    Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
+    Meteor.loginVisitor();
+    console.log(Meteor.userId());
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+    Template.hello.helpers({
+        counter: function () {
+            return Session.get("counter");
+        }
+    });
 
-  Template.allUsers.helpers({
-    users: function () {
-      return Meteor.users.find().fetch();
-    }
-  });
+    Template.hello.events({
+        'click button': function () {
+            // increment the counter when button is clicked
+            Session.set("counter", Session.get("counter") + 1);
+            var score = Session.get("counter");
+            Meteor.call('incrementScore', score);
+        }
+    });
+
+    Template.allUsers.helpers({
+        users: function () {
+            return Meteor.users.find().fetch();
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  //Meteor.loginVisitor();
-  //console.log(Meteor.userId());
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    //Meteor.loginVisitor();
+    //console.log(Meteor.userId());
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 
-  // add template method to set user name 
-  //Meteor.
+    // add method to set user name
+    //Meteor.
+
+    // method to set user score in db
+    Meteor.methods({
+        incrementScore: function (score) {
+            console.log('Increment score: ' + Meteor.userId());
+            //      Meteor.users.update(
+            //          { _id: Meteor.userId() },
+            //          { $inc: { 'score': 1 } }
+            //      );
+            Meteor.users.update(
+                { _id: Meteor.userId() },
+                { $set: { 'profile.score': score } }
+            );
+        }
+    });
 }
