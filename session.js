5f9ce3f9701a8b600c1fcbc39cf81e3fb57c6602
Brylie Christopher Oxley <brylie@gnumedia.org>
2014-12-10 19:57:23 +0200

    trying to auto-fill username field

--- session.js
+++ session.js
@@ -2,8 +2,8 @@ if (Meteor.isClient) {
     // counter starts at 0
     Session.setDefault("counter", 0);
 
+    // Create an anonymous user
     Meteor.loginVisitor();
-    console.log(Meteor.userId());
 
     Template.hello.helpers({
         counter: function () {
@@ -14,51 +14,64 @@ if (Meteor.isClient) {
     Template.hello.events({
         'click button': function () {
             // increment the counter when button is clicked
-            Session.set("counter", Session.get("counter") + 1);
-            var score = Session.get("counter");
+            //Session.set("counter", Session.get("counter") + 1);
+            //var score = Session.get("counter");
             Meteor.call('incrementScore');
-        },
-        'keyup #name': function (event) {
-            var username = event.target.value;
-            Meteor.call('changeUserName', username);
         }
     });
 
     Template.allUsers.helpers({
         users: function () {
-            return Meteor.users.find().fetch();
+            return Meteor.users.find(
+                {},
+                {sort: {'profile.score': -1 }}
+            ).fetch();
+        }
+    });
+    Template.userName.events({
+        'keyup #name': function (event) {
+            var username = event.target.value;
+            Meteor.call('changeUserName', username);
+        }
+    });
+    Template.userName.helpers({
+        username: function () {
+            var username = Meteor.call('getUserName');
+            console.log('username is: ' + username);
         }
     });
 }
 
 if (Meteor.isServer) {
     //Meteor.loginVisitor();
-    //console.log(Meteor.userId());
     Meteor.startup(function () {
         // code to run on server at startup
     });
 
-    // TODO: add method to set user name
-    //Meteor.
-
-    // method to set user score in db
     Meteor.methods({
+        // Increment the score by one
         incrementScore: function () {
-            console.log('Increment score: ' + Meteor.userId());
             Meteor.users.update(
                 { _id: Meteor.userId() },
                 { $inc: { 'profile.score': 1 } }
             );
-            //            Meteor.users.update(
-            //                { _id: Meteor.userId() },
-            //                { $set: { 'profile.score': score } }
-            //            );
         },
+        // Change username to a given value
         changeUserName: function (username) {
             Meteor.users.update(
                 { _id: Meteor.userId() },
                 { $set: { 'username': username } }
             );
+        },
+        // retrieve the username
+        getUserName: function () {
+            var userId = Meteor.userId();
+            console.log('user ID: ' + userId);
+            var user = Meteor.users.find(userId).fetch();
+            console.log('user: ' + user);
+            var username = user[0].username;
+            console.log("username: " + username);
+            return username;
         }
     });
 }
